f728b5aff306ff9192df47879f482a38d4558e95
doublemove <zhongwh1978@gmail.com>
2018-12-25 17:25:07 +0800

    '3'

--- server.py
+++ server.py
@@ -10,14 +10,12 @@ class Myserver(SocketServer.BaseRequestHandler):
         conn = self.request
         conn.sendall(bytes([u"你好，我是机器人",'1']))
         print conn.getsockname()
-        while True:
-            time.sleep(1)
-            ret_bytes = conn.recv(1024)
-            ret_str = unicode(ret_bytes)
-            print ret_str
-            # if ret_str == "q":
-                # break
-            # conn.sendall(bytes(ret_str+"你好我好大家好"))
+        ret_bytes = conn.recv(1024)
+        ret_str = unicode(ret_bytes)
+        print ret_str
+        # if ret_str == "q":
+            # break
+        # conn.sendall(bytes(ret_str+"你好我好大家好"))
 
 if __name__ == "__main__":
     print(u'啥事')
